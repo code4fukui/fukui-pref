bd26fb0ff9ef206abfcc3f3d1c3e057b2ab8cdcc
Taisuke Fukuno <fukuno@jig.jp>
2021-12-24 22:32:21 +0900

    add sample app

--- fukuijizakana/make.js
+++ fukuijizakana/make.js
@@ -1,5 +1,6 @@
 import { CSV } from "https://js.sabae.cc/CSV.js";
 import { cutLast } from "./cutLast.js";
+import { CBOR } from "https://js.sabae.cc/CBOR.js";
 
 const data = CSV.toJSON(await CSV.fetch("org_fukuijizakana.csv"));
 for (const d of data) {
@@ -7,3 +8,4 @@ for (const d of data) {
   d.URL = "https://code4fukui.github.io/fukui-pref/fukuijizakana/img/" + fn;
 }
 await Deno.writeTextFile("fukuijizakana.csv", CSV.stringify(data));
+await Deno.writeFile("fukuijizakana.cbor", CBOR.encode(data));
